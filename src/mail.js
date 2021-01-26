30498d8b14067c681c22dd3d54c09c158aad7110
Leonardo Theodoro <simplespinheiro@gmail.com>
2021-01-26 13:08:09 -0300

    Initial commit

---
+++ src/mail.js
@@ -0,0 +1,31 @@
+const mailer = require('nodemailer');
+require('dotenv/config')
+
+module.exports = (email, name, subject, message) => {
+  const smtpTransport = mailer.createTransport({
+    host: process.env.MAIL_HOST,
+    port: process.env.MAIL_PORT,
+    secure: process.env.MAIL_SECURE,
+    auth: {
+      user: process.env.MAIL_USER,
+      pass: process.env.MAIL_PASS,
+    }
+  });
+
+  const mail = {
+    from: `${name} (${email})`,
+    to: process.env.MAIL_USER,
+    subject: `Acontece Construtora Site | ${subject}`,
+    text: message
+  }
+
+  return new Promise((resolve, reject) => {
+    smtpTransport.sendMail(mail).then(response => {
+      smtpTransport.close();
+      return resolve(response);
+    }).catch(error => {
+      smtpTransport.close();
+      return reject(error);
+    });
+  })
+}
---
+++ src/server.js
@@ -0,0 +1,25 @@
+const http = require('http');
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const mail = require('./mail');
+
+const app = express();
+app.use(cors());
+app.use(bodyParser.json());
+
+app.get('/', (request, response, next) => {
+  return response.json({message: 'Server working!!'});
+});
+
+app.post('/send', (request, response, next) => {
+  const {email, name, subject, message} = request.body;
+  
+  mail(email, name, subject, message).then(res => response.json(res)).catch(error => response.json(error))
+});
+
+const server = http.createServer(app);
+
+server.listen(3030);
+
+console.log('Server listening on 3030');
